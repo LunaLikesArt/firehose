804bc1d96d1a435fc6dc94531e15be820015cfe5
Jenny Traer <lunalikesart@gmail.com>
2016-01-16 16:18:17 +0000

    Add 'About' page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+Splurty::Application.routes.draw do
+  root 'quotes#index'
+  resources :quotes
+  get 'about', to: 'quotes#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -53,10 +57,11 @@
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-Splurty::Application.routes.draw do
-root 'quotes#index'
 
-# The priority is based upon order of creation: first created -> highest priority.
-resources :quotes
 
 end
+
+
+
+
+
